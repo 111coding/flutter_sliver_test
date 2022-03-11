75cde1ebac018eec755f53b2fa0df3e4f3c00b85
mark <mark@markui-MacBookPro.local>
2022-03-12 00:16:18 +0900

    Route Edit

--- lib/route/app_router.dart
+++ lib/route/app_router.dart
@@ -9,18 +9,20 @@ enum AppRouter {
 
 class AppNavigator {
   // 뒤로가기
-  void pop<T extends Object?>([T? result]) => Navigator.of(navigatorKey.currentState!.context).pop();
+  static void pop<T extends Object?>([T? result]) => Navigator.of(navigatorKey.currentState!.context).pop();
 }
 
 /// 따로 파일로 빼버리면 AppRouter 확장함수 이용 시 AppRouterExtension이 포함된 파일을 따로 import 시켜줘야함
 /// 함께있으면 AppRouter 만 import 하면 사용가능 (개발시 용이)
 extension AppRouterExtension on AppRouter {
   /// 라우팅(현재페이지 위에 push)
-  Future<T?> push<T extends Object?>({Object? arguments}) => Navigator.of(navigatorKey.currentState!.context).pushNamed(name, arguments: arguments);
+  Future<T?> push<T extends Object?>({Object? arguments}) => navigatorKey.currentState!.pushNamed(name, arguments: arguments);
 
   /// 라우팅(현재페이지 pop 후 push)
-  Future<T?> popPush<T extends Object?, TO extends Object?>(Object? result, Object? arguments) => Navigator.of(navigatorKey.currentState!.context).popAndPushNamed(name, result: result, arguments: arguments);
+  Future<T?> popPush<T extends Object?, TO extends Object?>(Object? result, Object? arguments) => navigatorKey.currentState!.popAndPushNamed(name, result: result, arguments: arguments);
 
   /// 라우팅(이전페이지 모두 pop 후 push)
-  Future<T?> popAllPush<T extends Object?>({Object? arguments}) => Navigator.of(navigatorKey.currentState!.context).pushNamedAndRemoveUntil(name, (route) => false, arguments: arguments);
+  Future<T?> popAllPush<T extends Object?>({Object? arguments}) => navigatorKey.currentState!.pushNamedAndRemoveUntil(name, (route) => false, arguments: arguments);
+
+  static void pop<T extends Object?>([T? result]) => Navigator.of(navigatorKey.currentState!.context).pop();
 }
