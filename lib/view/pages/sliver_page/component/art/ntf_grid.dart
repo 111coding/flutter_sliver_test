997438630a598d89d16e16d06dd7b36d0c0c87fa
mark <mark@markui-MacBookPro.local>
2022-03-16 17:20:36 +0900

    LayoutBuilder

--- lib/view/pages/sliver_page/component/art/ntf_grid.dart
+++ lib/view/pages/sliver_page/component/art/ntf_grid.dart
@@ -17,45 +17,49 @@ class NtfGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int temp = (MediaQuery.of(context).size.width / 200).floor();
-    int count = max(3, temp);
     return Container(
       constraints: const BoxConstraints(maxWidth: ConstValue.maxWidth),
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: GridView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        padding: EdgeInsets.zero,
-        itemCount: min(maxLength, ntfs.length),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: count,
-          childAspectRatio: 0.8 * 3 / count,
-          mainAxisExtent: 180, // 세로 최대크기
-          mainAxisSpacing: 10, // 세로 간격
-          crossAxisSpacing: 10, // 가로 간격
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          var n = ntfs[index];
-          return Column(
-            children: [
-              SizedBox(
-                width: 100,
-                child: NtfImage.asset(n.imgSrc),
-              ),
-              const SizedBox(height: 6),
-              Text(
-                "${n.charName} ${n.tokenId}",
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 3),
-              Text(
-                n.onwerName,
-                style: const TextStyle(color: Colors.grey),
-              ),
-            ],
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          int renderCount = (constraints.maxWidth / 150).floor();
+          int renderCountMin = max(3, renderCount);
+          return GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            itemCount: min(maxLength, ntfs.length),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: renderCountMin,
+              childAspectRatio: 0.8 * 3 / renderCountMin,
+              mainAxisExtent: 180, // 세로 최대크기
+              mainAxisSpacing: 10, // 세로 간격
+              crossAxisSpacing: 10, // 가로 간격
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              var n = ntfs[index];
+              return Column(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: NtfImage.asset(n.imgSrc),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    "${n.charName} ${n.tokenId}",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 3),
+                  Text(
+                    n.onwerName,
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ],
+              );
+            },
           );
         },
       ),
