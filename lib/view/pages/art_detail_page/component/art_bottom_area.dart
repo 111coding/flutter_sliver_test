ea07ff53a2155c844d89992c9f764f8497e480e6
mark <mark@markui-MacBookPro.local>
2022-03-16 17:35:49 +0900

    ArtBottomArea

--- lib/view/pages/art_detail_page/component/art_bottom_area.dart
+++ lib/view/pages/art_detail_page/component/art_bottom_area.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:sliver_test/domain/model/collectable.dart';
+import 'package:sliver_test/dummy_data/value.dart';
 import 'package:sliver_test/view/components/image/collectable_image.dart';
 import 'package:sliver_test/view/components/line.dart';
 
@@ -29,40 +30,41 @@ class ArtBottomArea extends StatelessWidget {
     return Container(
       height: height,
       color: Colors.white,
+      constraints: const BoxConstraints(maxWidth: ConstValue.maxWidth),
       child: PageView.builder(
         onPageChanged: onPageChanged,
         itemCount: (datas.length / count).ceil(),
         itemBuilder: (context, pageIndex) {
-          return GridView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            padding: EdgeInsets.zero,
-            itemCount: min(count, datas.length),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: count,
-              childAspectRatio: 0.8,
-              mainAxisExtent: height, // 세로 최대크기
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              int absIndex = index + pageIndex * count;
-              var c = datas[absIndex];
-              return Column(
-                children: [
-                  Line(color: absIndex == currentIndex ? Colors.black : Colors.grey),
-                  Expanded(
-                    child: Center(
-                      child: GestureDetector(
-                        onTap: () {
-                          onItemTap(absIndex);
-                        },
-                        child: SizedBox(
-                          width: 40,
-                          child: CollectableImage.asset(c.ntfs.first.imgSrc),
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              int renderCount = (constraints.maxWidth / 80).floor();
+              int renderCountMin = max(5, renderCount);
+
+              return Row(
+                children: List.generate(renderCountMin, (index) {
+                  int absIndex = index + pageIndex * count;
+                  var c = datas[absIndex];
+                  return Expanded(
+                    child: Column(
+                      children: [
+                        Line(color: absIndex == currentIndex ? Colors.black : Colors.grey),
+                        Expanded(
+                          child: Center(
+                            child: GestureDetector(
+                              onTap: () {
+                                onItemTap(absIndex);
+                              },
+                              child: SizedBox(
+                                width: 40,
+                                child: CollectableImage.asset(c.ntfs.first.imgSrc),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  )
-                ],
+                  );
+                }),
               );
             },
           );
